a9f2e57bac33448d0c9b1f980b7d1ff558cfbf2b
A1iHassan <ali012wkout@gmail.com>
2025-01-03 23:34:10 +0200

    set up the appwrite service credentials

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import * as sdk from "https://deno.land/x/appwrite/mod.ts";
+import "jsr:@std/dotenv/load";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+let client = new sdk.Client();
+
+client
+    .setEndpoint(Deno.env.get("END_POINT")) // Your API Endpoint
+    .setProject(Deno.env.get("PROJECT_ID")) // Your project ID
+    .setKey(Deno.env.get("SECRET_KEY")) // Your secret API key
+;
+
+
+
+console.log("done");
