af11d61e3b0f281525aa336036f48bcbb8a7b05d
A1iHassan <ali012wkout@gmail.com>
2025-01-17 21:57:36 +0200

    created the log in api

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import helmet from "helmet";
-import { genSalt, hash } from "bcrypt-ts";
+// import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { genSalt, hash, compare } from "bcrypt-ts";
 import prisma from "./db";
 import { Validator } from "jsonschema";
 import {
@@ -12,6 +14,7 @@ import {
   isValidPhoneNumber,
 } from "./utils/validation";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -28,6 +31,26 @@ const signUpSchema = {
     password: { type: "strig" },
   },
   required: ["name", "email", "phoneNumber", "password"],
+  additionalProperties: false,
+};
+
+const phoneLogIn = {
+  type: "object",
+  properties: {
+    phoneNumber: { type: "string" },
+    password: { type: "string" },
+  },
+  required: ["phoneNumber", "password"],
+  additionalProperties: false,
+};
+
+const emailLogIn = {
+  type: "object",
+  properties: {
+    email: { type: "string" },
+    password: { type: "string" },
+  },
+  required: ["email", "password"],
   additionalProperties: false,
 };
 
@@ -71,9 +94,52 @@ app.post(
   }
 );
 
-app.post("/api/v1/login", (req: Request, res: Response) => {
-  res.send("log in route");
-});
+app.post(
+  "/api/v1/login/phone",
+  async (req: Request, res: Response): Promise<Response | any> => {
+    const data = req.body;
+    if (!v.validate(data, phoneLogIn).valid) {
+      return res.status(400).json({ message: "invalid request body" });
+    }
+    if (!isValidPhoneNumber) {
+      return res.status(400).json({ message: "invalid phone number" });
+    }
+    const { phoneNumber, password } = data;
+    const user = await prisma.user.findFirst({
+      where: { phoneNumber },
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "wrong phone number or password" });
+    }
+    if (await compare(password, user.password)) {
+      const payload = {
+        name: user.name,
+        id: user.id,
+        password: user.password,
+      };
+      const secret = process.env.TOKEN_KEY;
+      // const token = jwt.sign(
+      //   payload,
+      //   "hjs82KSD9!@sldj*&sDla%0Ksjeh4@3*DFJKL3kjd83s",
+      //   {
+      //     expiresIn: "1h",
+      //   }
+      // );
+    }
+  }
+);
+
+app.post(
+  "/api/v1/login/email",
+  async (req: Request, res: Response): Promise<Response | any> => {
+    const data = req.body;
+    if (!v.validate(data, emailLogIn).valid) {
+      return res.status(400).json({ message: "invalid request body" });
+    }
+  }
+);
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
